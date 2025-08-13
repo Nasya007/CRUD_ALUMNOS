4fd59c528b599745c42507597c725e5693f0e64d
Nasya C <pereznasya01@gmail.com>
2025-08-13 13:42:24 -0600

    Correcion

--- StoredProcedures/StoredProcedures.sql
+++ StoredProcedures/StoredProcedures.sql
@@ -1,7 +1,8 @@
 CREATE PROCEDURE sp_ObtenerAlumnos
 AS
 BEGIN
-	SELECT ID_Alumno, NoControl, Nombres, ApellidoPaterno, ApellidoMaterno, Activo, FechaDeNacimiento, ID_Carrera
-	FROM Tb_Alumno 
-	ORDER BY ApellidoPaterno ASC
+	SELECT ID_Alumno, NoControl, Nombres, ApellidoPaterno, ApellidoMaterno, Activo, FechaDeNacimiento, c.Carrera
+	FROM Tb_Alumno a
+	INNER JOIN dbo.Tb_Carrera c ON a.ID_CARRERA = c.ID_Carrera
+	ORDER BY ApellidoPaterno ASC;
 END

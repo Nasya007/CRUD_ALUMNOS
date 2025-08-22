3998ee31c4cd6c51efeca41e6fe83bb3eb182873
Nasya C <pereznasya01@gmail.com>
2025-08-22 09:02:40 -0600

    Array a Json

---
+++ DbEscuela/Models/Participante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DbEscuela.Models
+{
+    public class Participante
+    {
+        public string Nombre { get; set; }
+        public string Grupo { get; set; }
+        public string Regalo { get; set; }
+        public string Email { get; set; }
+    }
+}
